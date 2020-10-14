00baa742d5a48459e4da69926a44aa1c25250ed3
Dmitriy Proshutin <dmitry.proshutin@gmail.com>
2020-10-14 22:44:03 +0600

    The main classes for creating a maze are set.

---
+++ MazeKZ/Maze.cs
@@ -0,0 +1,21 @@
+﻿using MazeKZ.Cells;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MazeKZ
+{
+    public class Maze
+    {
+        public List<Cell> Cells { get; set; }
+        public int Width { get; set; }
+        // public int WidthGet => Cells.Max(cell => cell.X);
+
+        public int Height { get; set; }
+        public Maze()
+        {
+            Cells = new List<Cell>();
+        }
+    }
+}
