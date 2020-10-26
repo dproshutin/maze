657438991d7fd28d8e5879e22979097ac5f74587
Dmitriy Proshutin <dmitry.proshutin@gmail.com>
2020-10-27 00:01:56 +0600

    Possibility to change maze's size is added.

--- MazeKZ/ConsoleUIHelper.cs
+++ MazeKZ/ConsoleUIHelper.cs
@@ -1,16 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using MazeKZ.CustomExceptions;
 
 namespace MazeKZ
 {
     class ConsoleUIHelper
     {
-        public void Play()
+        public void ConfigurateMaze()
         {
-            var mazeGenerator = new MazeGenerator();
-            var maze = mazeGenerator.GenerateSmart(20, 10);
+            var width = ReadInt("Какой ширины должен быть лабиринт?");
+            var height = ReadInt("А какой он должен быть высоты?");
 
+
+            var mazeGenerator = new MazeGenerator();
+            var maze = mazeGenerator.GenerateSmart(width, height);
+
+            Play(maze);
+        }
+        public void Play(Maze maze)
+        {
             var draw = new Drawer();
             draw.DrawMaze(maze);
 
@@ -46,6 +55,22 @@ namespace MazeKZ
             }
 
             Console.WriteLine("Goodbye");
+        }
+
+        public int ReadInt(string message)
+        {
+            Console.WriteLine(message);
+            var str = Console.ReadLine();
+            var number = -1;
+
+            number = Int32.Parse(str);
+            while(!Int32.TryParse(str, out number) || number < 0)
+            {
+                Console.WriteLine("Это было не число, ну или оно было слишком маленькое.");
+                Console.WriteLine("Нужно число больше 0. Попробуй еще раз.");
+                str = Console.ReadLine();
+            }
+            return number;
         }
     }
 }
---
+++ MazeKZ/CustomExceptions/NegativeException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MazeKZ.CustomExceptions
+{
+    public class NegativeException : Exception
+    {
+        public override string Message => "Числа не могут быть меньше нуля";
+    }
+}
